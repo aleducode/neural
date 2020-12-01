00ff1e7142c93ba6344250ae56fb3ead45684ab5
ducode <ducode@outlook.com>
2020-12-01 09:00:55 -0500

    add filter by date.

--- neural/training/admin.py
+++ neural/training/admin.py
@@ -12,6 +12,7 @@ class UserTrainingInline(admin.TabularInline):
 
 @admin.register(Slot)
 class SlotAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
     list_filter = ['date', 'hour_init']
     list_display = ['pk', '__str__', 'date', 'hour_init', 'available_places', 'training_type']
     inlines = [UserTrainingInline]
