6c3f64b60d316a045e26b0a2f4bd8adaf7c09b73
Yesid9126 <yesidf26@gmail.com>
2023-08-23 11:09:38 -0500

    fixes

--- neural/users/tasks.py
+++ neural/users/tasks.py
@@ -8,11 +8,10 @@ from neural.users.models import UserMembership
 
 @celery_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-
     sender.add_periodic_task(
         crontab(day_of_week='*', hour=00, minute=00),
         check_user_memberships,
-        name="check_notifications_jail_v2"
+        name="check_user_memberships"
     )
 
 
